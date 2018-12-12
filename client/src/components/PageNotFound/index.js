2b939ae0febf4d41373da75996b6b3e2e6b42611
Joe Friel <joseph.s.friel@gmail.com>
2018-12-12 17:58:57 +0000

    Add color theme and fonts > rel issue #11

--- client/src/components/PageNotFound/index.js
+++ client/src/components/PageNotFound/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 
 const PageNotFound = () => (
-  <div className="flex flex-column">
+  <div className="flex flex-column mp-primary-color">
     <h1>404</h1>
     <h2>Sorry page not found for now</h2>
+    <p className="mp-black-color">Please return to the home page</p>
   </div>
 )
 

4869e525d8008619dcf047987e9d72c26954b922
Joe Friel <joseph.s.friel@gmail.com>
2018-12-12 16:43:43 +0000

    Add tachyons with its default normalise css > rel issue #11

--- client/src/components/PageNotFound/index.js
+++ client/src/components/PageNotFound/index.js
@@ -1,8 +1,7 @@
 import React from "react"
-import "./index.css"
 
 const PageNotFound = () => (
-  <div className="pageNotFound">
+  <div className="flex flex-column">
     <h1>404</h1>
     <h2>Sorry page not found for now</h2>
   </div>

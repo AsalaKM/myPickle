b3e9aed98659d488b4c7e424f1f16d06f60969c1
Joe Friel <joseph.s.friel@gmail.com>
2018-12-13 16:42:13 +0000

    Add initial styling to register stage one > rel issue #23

--- client/src/Components/Layouts/Register/Register.style.js
+++ client/src/Components/Layouts/Register/Register.style.js
@@ -27,4 +27,35 @@ const Button = styled.button.attrs({
   }
 `
 
-export { Intro, Button }
+const CircleLarge = styled.div.attrs({
+  className: "mp-b--primary-color",
+})`
+  border-style: solid;
+  position: absolute;
+  border-radius: 50%;
+  border-width: 1px;
+  left: -40vw;
+  width: 80vw;
+  height: 80vw;
+`
+
+const CircleMed = styled(CircleLarge)`
+  width: 60vw;
+  height: 60vw;
+  left: -30vw;
+  top: 10vw;
+`
+
+const CircleSm = styled(CircleLarge)`
+  width: 40vw;
+  height: 40vw;
+  left: -20vw;
+  top: 20vw;
+`
+
+const CirclesContainer = styled.div`
+  position: relative;
+  height: 80vw;
+`
+
+export { Intro, Button, CircleLarge, CircleMed, CirclesContainer, CircleSm }

8184232e3180fa61476301f967817c0723a56d05
Joe Friel <joseph.s.friel@gmail.com>
2018-12-13 18:37:24 +0000

    Weird github bug creating different component folders

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import App from "./Components/App/index"
+import App from "./TotalComponents/App/index"
 import * as serviceWorker from "./serviceWorker"
-// import "./index"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 

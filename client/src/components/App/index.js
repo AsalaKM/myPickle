a6a3e9edc9a340f7f467011d686c5907f0f0cb1b
Simon Dupree <dupreenator@gmail.com>
2018-12-13 13:07:51 +0000

    landing page component and styled component setup >relates 21

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import PageNotFound from "../PageNotFound"
+import LandingPage from "../Layouts/LandingPage/Landing"
 
 const App = () => (
   <Router>
     <Switch>
+      <Route path="/" exact component={LandingPage} />
       <Route path="*" component={PageNotFound} />
     </Switch>
   </Router>
---
+++ client/src/components/Layouts/LandingPage/Landing.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import {
+  Heading,
+  Container,
+  Logo,
+  Paragraph,
+  TextLink,
+  ButtonWrapper,
+  Button,
+} from "./LandingStyle"
+
+const LandingPage = () => (
+  <Container>
+    <Logo />
+    <Heading>My Pickle</Heading>
+    <Paragraph>
+      My Pickle brings together therapists and those who want their help. Find therapists, read
+      articles written by them, and get the support you need.
+    </Paragraph>
+    <TextLink to="/">Find out more about My Pickle</TextLink>
+    <ButtonWrapper>
+      <Button to="/">Find Support</Button>
+      <Button to="/">Offer Support</Button>
+    </ButtonWrapper>
+  </Container>
+)
+
+export default LandingPage

121b9c20d00ea5e2570e2f2025b793e5e6ac477c
Joe Friel <joseph.s.friel@gmail.com>
2018-12-13 18:23:09 +0000

    Fix index labelling bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./Components/App/index"
 import * as serviceWorker from "./serviceWorker"
-import "./index"
+// import "./index"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 

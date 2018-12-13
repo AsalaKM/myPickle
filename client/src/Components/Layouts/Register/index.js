abd9e8c1d71cd8a14d3665f9c7e49365521533c8
Joe Friel <joseph.s.friel@gmail.com>
2018-12-13 11:42:16 +0000

    Set up initial overall register component > rel #23

---
+++ client/src/Components/Layouts/Register/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Register extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Register page!!!</h1>
+        <p>Hello - register please</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Register
--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import PageNotFound from "../PageNotFound"
+import PageNotFound from "../Layouts/PageNotFound/index"
+import Register from "../Layouts/Register/index"
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="*" component={PageNotFound} />
+      <Route path="/register" exact component={Register} />
+      <Route component={PageNotFound} />
     </Switch>
   </Router>
 )

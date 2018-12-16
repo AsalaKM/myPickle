07af5538576654090bc1f7deab92cb04ffab6c1e
hshahwan <haneen.shahwan9@gmail.com>
2018-12-16 21:57:35 +0200

    fixed Eslint

--- client/src/Components/Layouts/Login/Login.style.js
+++ client/src/Components/Layouts/Login/Login.style.js
@@ -1,58 +1,49 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
-export const LoginPage = styled.div
-`
+export const LoginPage = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-
 `
 
-export const Form =styled.form
+export const Form = styled.form`
+  text-align: left;
+  margin-left: 50px;
+  border: none;
 `
-text-align: left;
-margin-left: 50px;
-border:none;
-  `
 
-export const HomeIcon =styled.img
-`
+export const HomeIcon = styled.img`
   height: 30px;
   width: 60px;
   left: 0;
 `
-export const Input = styled.input
-`
-border: none;
-height: 50px;
-border-radius: 10px;
-width: 240px;
-display: block;
-text-align: center;
+export const Input = styled.input`
+  border: none;
+  height: 50px;
+  border-radius: 10px;
+  width: 240px;
+  display: block;
+  text-align: center;
 `
 
-export const Logo =styled.img
-
-` width: 200px;
+export const Logo = styled.img`
+  width: 200px;
   margin-left: 60px;
   margin-top: 50px;
 `
 
-export const Title = styled.h2
+export const Title = styled.h2`
+  text-align: left;
+  margin-left: 120px;
+  display: block;
 `
-text-align: left;
-margin-left: 120px;
-display: block;
+export const Button = styled.button`
+  border: none;
+  height: 59px;
+  border-radius: 10px;
+  width: 250px;
+  display: block;
+  text-align: center;
+  margin-left: 50px;
+  background: #6eeb26;
 `
-export const Button = styled.button
-`
-border: none;
-height: 59px;
-border-radius: 10px;
-width: 250px;
-display: block;
-text-align: center;
-margin-left: 50px;
-background:#6eeb26;
-`
-
--- client/src/Components/Layouts/Login/index.js
+++ client/src/Components/Layouts/Login/index.js
@@ -1,66 +1,60 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import home from '../../../assets/images/home.svg'
-import logo from '../../../assets/images/logo.jpg'
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import home from "../../../assets/images/home.svg"
+import logo from "../../../assets/images/logo.jpg"
 
-
-import { LoginPage,Form,HomeIcon,Logo,Title,Input,Button } from './Login.style.js'
+import { LoginPage, Form, HomeIcon, Logo, Title, Input, Button } from "./Login.style.js"
 
 class Login extends Component {
   state = {
-    email : '',
-    password : ''
+    email: "",
+    password: "",
   }
-  onChange =(e) => {
+  onChange = e => {
     this.setState({
-      [e.target.name]: e.target.value
-     });
+      [e.target.name]: e.target.value,
+    })
   }
 
   clickHome = () => {
-    this.props.history.push('/');
-  };
+    this.props.history.push("/")
+  }
 
   render() {
-
     return (
       <LoginPage>
+        <HomeIcon src={home} onClick={this.clickHome} />
 
-        <HomeIcon  src={ home } onClick={ this.clickHome }></HomeIcon>
+        <Logo src={logo} />
 
-       <Logo src={ logo }></Logo>
+        <Title>myPikle</Title>
 
-      <Title >myPikle</Title >
+        <Form>
+          <Input
+            type="email"
+            placeholder="Your Email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <br />
 
-      <Form>
-        <Input
-        type='email'
-        placeholder='Your Email'
-        name='email'
-        value={this.state.email}
-        onChange={this.onChange}
-        >
-        </Input>
-        <br/>
+          <Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+          <br />
+        </Form>
 
-        <Input
-        type='password'
-        placeholder='Password'
-        name='password'
-        value={this.state.password}
-        onChange={this.onChange}
-         >
-         </Input>
-        <br/>
-      </Form>
+        <Button>Login</Button>
 
-      <Button>Login</Button>
-
-      <Link to="/register">Create new acccount!</Link>
-
+        <Link to="/register">Create new acccount!</Link>
       </LoginPage>
     )
   }
 }
 
-export default Login;
+export default Login

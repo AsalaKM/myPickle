9e9ca4f8e3c67f37fa8562798d1fc211abcc2820
hshahwan <haneen.shahwan9@gmail.com>
2018-12-16 21:59:33 +0200

    fiexed indentation

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -2,8 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import PageNotFound from "../Layouts/PageNotFound/index"
 import Register from "../Layouts/Register/index"
-import Login from '../Layouts/Login/index'
-
+import Login from "../Layouts/Login/index"
 
 const App = () => (
   <Router>

bd1c51381437bdde95d41c17b1804e19891f90e4
hshahwan <haneen.shahwan9@gmail.com>
2018-12-16 18:51:04 +0200

    add styled to loginComponent

--- client/src/Components/Layouts/Login/index.js
+++ client/src/Components/Layouts/Login/index.js
@@ -1,38 +1,64 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import home from '../../../assets/images/home.svg'
+import logo from '../../../assets/images/logo.jpg'
+
+
+import { LoginPage,Form,HomeIcon,Logo,Title,Input,Button } from './Login.style.js'
 
 class Login extends Component {
   state = {
     email : '',
     password : ''
   }
-  onChange(e) {
+  onChange =(e) => {
     this.setState({
       [e.target.name]: e.target.value
      });
   }
 
+  clickHome = () => {
+    this.props.history.push('/');
+  };
 
   render() {
+
     return (
-      <form>
-        <input
+      <LoginPage>
+
+        <HomeIcon  src={ home } onClick={ this.clickHome }></HomeIcon>
+
+       <Logo src={ logo }></Logo>
+
+      <Title >myPikle</Title >
+
+      <Form>
+        <Input
         type='email'
         placeholder='Your Email'
         name='email'
         value={this.state.email}
         onChange={this.onChange}
-        />
+        >
+        </Input>
         <br/>
-        <input
+
+        <Input
         type='password'
         placeholder='Password'
         name='password'
         value={this.state.password}
         onChange={this.onChange}
-         />
+         >
+         </Input>
         <br/>
-        <button>Login</button>
-      </form>
+      </Form>
+
+      <Button>Login</Button>
+
+      <Link to="/register">Create new acccount!</Link>
+
+      </LoginPage>
     )
   }
 }

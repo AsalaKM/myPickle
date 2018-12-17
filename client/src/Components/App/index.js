611248789326fab97871dc034588837ef3187682
hshahwan <haneen.shahwan9@gmail.com>
2018-12-17 13:19:03 +0200

    add style to dashbord and add route to application

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -2,13 +2,16 @@ import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import PageNotFound from "../Layouts/PageNotFound/index"
 import Register from "../Layouts/Register/index"
-import Login from "../Layouts/Login/index"
+import Login from "../Layouts/Login"
+import Dashboard from "../Layouts/Dashboard"
+
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/dashboard" exact component={Dashboard} />
       <Route component={PageNotFound} />
     </Switch>
   </Router>
--- client/src/Components/Layouts/Dashboard/index.js
+++ client/src/Components/Layouts/Dashboard/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react"
+import  {Button,Container,Logout }  from "./Dashboard.style.js"
+
 
 
 class Dashboard extends Component {
 
   render() {
-    return (
+   return (
+<Container>
+  <h2>Welcome back, Haneen</h2>
 
-    )
+ <Button > Edit Profile </Button>
+ <br/>
+ <Button > Create Blog Post </Button>
+ <br/>
+ <Button > Browse Profiles</Button>
+ <br/>
+ <Button >Browse Blog Posts</Button>
+ <br/>
+
+ <Logout>Logout</Logout>
+</Container>
+    );
 
   }
 }

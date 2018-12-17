5b971b90d283befe01e757bef63e96f4b0d14b57
Joe Friel <joseph.s.friel@gmail.com>
2018-12-17 16:00:49 +0000

    Add user model inputs for registration > rel issue #23

--- client/src/Components/Layouts/Register/RegisterStepTwo.js
+++ client/src/Components/Layouts/Register/RegisterStepTwo.js
@@ -9,12 +9,56 @@ export default class RegisterIntro extends React.Component {
       <React.Fragment>
         <div>
           <header>
-            <h4>Contact Name</h4>
+            <h4>Contact Name:</h4>
             <input
               type="text"
               name="name"
               onChange={handleChange}
               value={answers["name"] ? answers["name"] : ""}
+            />
+          </header>
+        </div>
+        <div>
+          <header>
+            <h4>Contact Email:</h4>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={answers["email"] ? answers["email"] : ""}
+            />
+          </header>
+        </div>
+        <div>
+          <header>
+            <h4>Contact Password:</h4>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={answers["password"] ? answers["password"] : ""}
+            />
+          </header>
+        </div>
+        <div>
+          <header>
+            <h4>Confirm Password:</h4>
+            <input
+              type="password"
+              name="password2"
+              onChange={handleChange}
+              value={answers["password2"] ? answers["password2"] : ""}
+            />
+          </header>
+        </div>
+        <div>
+          <header>
+            <h4>Phone Number:</h4>
+            <input
+              type="text"
+              name="phone"
+              onChange={handleChange}
+              value={answers["phone"] ? answers["phone"] : ""}
             />
           </header>
         </div>

278f4908d2ffef5b23a02b2e6d302608c56aef4e
Simon Dupree <dupreenator@gmail.com>
2018-12-18 17:14:24 +0000

    store profile answers query setup relates #23

--- server/api/controllers/registerUserController.js
+++ server/api/controllers/registerUserController.js
@@ -3,26 +3,35 @@ const router = express.Router()
 
 // load queries
 const registerUser = require("../../database/queries/registerUser")
-
 const registerProfile = require("../../database/queries/registerProfile")
-
 const getSupportType = require("../../database/queries/getSupportType")
+const getProfileAnswers = require("../../database/queries/getProfileAnswers")
+const storeAnswers = require("../../database/queries/storeProfileAnswers")
 
 router.post("/", async (req, res) => {
   console.log("REQBODY", req.body)
   // create user object
   const { name, email, phone, password } = req.body
-
-  const supportTypeID = await getSupportType(req.body).catch(err => console.log(err))
-
-  const newUser = await registerUser(name, email, phone, password).catch(err => console.log(err))
+  // get support type id (either therapist or general)
+  const supportTypeID = await getSupportType(req.body).catch(err =>
+    console.log("err getting support type id", err)
+  )
+  // register a new user
+  const newUser = await registerUser(name, email, phone, password).catch(err =>
+    console.log("err registering new user", err)
+  )
   console.log("newUser", newUser)
 
+  // register new profile
   const newProfile = await registerProfile(supportTypeID, newUser._id, false).catch(err =>
-    console.log(err)
+    console.log("error creating new profile", err)
   )
-
   console.log("newProfile", newProfile)
+  // create profile answers object
+  const profileAnswers = await getProfileAnswers(req.body)
+  // store those answers in database
+  const saveProfileAnswers = await storeAnswers(newProfile._id, profileAnswers)
+  console.log("stored Profile Answers", saveProfileAnswers)
 })
 
 module.exports = router

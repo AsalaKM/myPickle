46f16cc4b41818a31a5c3c3708459899734e889a
Simon Dupree <dupreenator@gmail.com>
2018-12-22 19:12:46 +0100

    target clients edit profile component render setup

--- client/src/Components/Layouts/EditProfile/TargetClients.js
+++ client/src/Components/Layouts/EditProfile/TargetClients.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react"
 import axios from "axios"
 
+import { Button, Answers, Intro } from "../Register/Register.style"
+import { CheckboxField } from "../../Common/Questions/Questions.style"
+
 class TargetClients extends Component {
   state = {
     profileId: "",
     targetAnswers: null,
     targetQuestions: null,
+    unanswered: [],
+    errors: {},
+    loaded: false,
   }
 
   componentDidMount() {
@@ -13,8 +19,13 @@ class TargetClients extends Component {
     const id = pathName.split("/")[3]
 
     axios
-      .get(`/get-questions/targets-clients/${id}`)
-      .then(questions => this.setState({ targetQuestions: questions.data }))
+      .get(`/get-questions/target-clients/${id}`)
+      .then(questions =>
+        this.setState({
+          targetQuestions: questions.data,
+          loaded: true,
+        })
+      )
       .catch(err => console.log(err))
 
     axios
@@ -24,9 +35,56 @@ class TargetClients extends Component {
   }
 
   render() {
+    const { loaded, targetQuestions, targetAnswers } = this.state
+    console.log(targetQuestions)
+
+    if (!loaded) {
+      return (
+        <Intro>
+          <h2 className="tc mp-primary-color">Loading Your Details...</h2>
+        </Intro>
+      )
+    }
     return (
       <React.Fragment>
-        <h4>{this.state.profileId}</h4>
+        <Intro>
+          <h2 className="tc mp-primary-color">Your Details</h2>
+        </Intro>
+        <Answers>
+          {targetQuestions
+            .filter(question => question.inputType === "checkbox")
+            .map((question, index) => (
+              <CheckboxField>
+                <header>
+                  <h4>
+                    {question.questionText}
+                    {question.isRequired ? "*" : ""}
+                  </h4>
+                  <p>{question.helperText}</p>
+                </header>
+                <div>
+                  {question.options.map(option => {
+                    const uniqueId = option + question._id
+                    return (
+                      <label htmlFor={uniqueId} key={Math.random()}>
+                        <input
+                          value={option}
+                          type="checkbox"
+                          id={uniqueId}
+                          name={question._id}
+                          // checked={
+                          //   targetAnswers[question._id] &&
+                          //   targetAnswers[question._id].includes(option)
+                          // }
+                        />
+                        <p>{option}</p>
+                      </label>
+                    )
+                  })}
+                </div>
+              </CheckboxField>
+            ))}
+        </Answers>
       </React.Fragment>
     )
   }

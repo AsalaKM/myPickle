8f7fb40b291ed1229363fa40388ab3cb6b8444f5
Simon Dupree <dupreenator@gmail.com>
2018-12-23 23:59:00 +0100

    axios require deleted in update target controller

--- server/api/controllers/updateTargetClientsDetails.js
+++ server/api/controllers/updateTargetClientsDetails.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const axios = require("axios")
+
 // load query
 const updateTargetClientsDetails = require("../../database/queries/editProfile/updateTargetClients")
 

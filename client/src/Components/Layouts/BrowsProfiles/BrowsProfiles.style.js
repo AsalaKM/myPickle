3f6618030aaaab0f7129867f43ee0a37fd39e9cb
hshahwan <haneen.shahwan9@gmail.com>
2018-12-28 18:01:21 +0200

    create Filter button and makeup by CSS and adding routes in App

---
+++ client/src/Components/Layouts/BrowsProfiles/BrowsProfiles.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+display: flex;
+margin: 0 auto;
+margin-top: 100px;
+height: 45px;
+width: 105px;
+margin-right: 25px;
+border-radius: 20px;
+border-color: #6eeb2e;
+background: #f8fbfa;
+border-width: .5px;
+color: #6eeb2e;
+
+  `;
--- client/src/Components/Layouts/BrowsProfiles/index.js
+++ client/src/Components/Layouts/BrowsProfiles/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import { Button } from './BrowsProfiles.style';
 
 class BrowsProfiles extends Component {
   render() {
     return (
-
+      <div>
+        <Button>FILTER</Button>
+      </div>
 
     );
   }

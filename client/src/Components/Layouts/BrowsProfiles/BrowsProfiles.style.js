f15cf195e4de2538a4b91f6013f2cb4ff6abc699
hshahwan <haneen.shahwan9@gmail.com>
2018-12-29 12:05:26 +0200

    create Profile card that contain the bio location and wellness area and link to more details about the therpaist

--- client/src/Components/Layouts/BrowsProfiles/BrowsProfiles.style.js
+++ client/src/Components/Layouts/BrowsProfiles/BrowsProfiles.style.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
+
 export const Button = styled.button`
 display: flex;
 margin: 0 auto;
 margin-top: 100px;
 height: 45px;
 width: 105px;
-margin-right: 25px;
+margin-right: 37px;
 border-radius: 20px;
 border-color: #6eeb2e;
 background: #f8fbfa;
@@ -14,3 +15,24 @@ border-width: .5px;
 color: #6eeb2e;
 
   `;
+
+export const Card = styled.div`
+  display:flex;
+  flex-direction: column;
+  border-radius: 41px;
+  background: #cccccca3;
+  width: 85%;
+  height: 108px;
+  justify-content: center;
+  align-items: center;
+  font-size: 11px;
+  margin-left: 25px;
+  margin-top: 8px;
+
+  `;
+
+export const Avatar = styled.img`
+  height:55px;
+  width:55px;
+  `;
+
--- client/src/Components/Layouts/BrowsProfiles/index.js
+++ client/src/Components/Layouts/BrowsProfiles/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import work from '../../../assets/images/work.jpeg';
 
-import { Button } from './BrowsProfiles.style';
+import { Button, Card, Avatar } from './BrowsProfiles.style';
 
 class BrowsProfiles extends Component {
   render() {
     return (
       <div>
+
         <Button>FILTER</Button>
+
+        <Card>
+          <Avatar src={work} />
+          <span>Description </span>
+          <span>Location</span>
+          <span> wellness area</span>
+          <a href="#">
+            <span>more details</span>
+          </a>
+        </Card>
+
       </div>
 
     );

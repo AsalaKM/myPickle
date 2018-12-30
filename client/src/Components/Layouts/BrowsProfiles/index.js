7d281108f6ef2138b98bda4582d805b32341e346
hshahwan <haneen.shahwan9@gmail.com>
2018-12-30 12:37:47 +0200

    update the component to logical and fetch the data to get it from back end

--- client/src/Components/Layouts/BrowsProfiles/index.js
+++ client/src/Components/Layouts/BrowsProfiles/index.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import CardElements from './Card'
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+import CardElements from './Card';
+import { Button } from './BrowsProfiles.style';
 
 
 class BrowsProfiles extends Component {
+  state = {
+    results: [],
+  };
+
+  componentDidMount() {
+        axios
+          .get('/api/v1/profilesList')
+          .then((res) => {
+            const info = res.data;
+            this.setState({ results: info });
+          })
+          .catch((error) => {
+            console.log('error:', error);
+          });
+      }
+
+
   render() {
+    const { results } = this.state;
+    if (results.length !== 0) {
     return (
-      <Button>FILTER</Button>
-      <CardElements />
+        <div>
+          <Button>FILTER</Button>
+        <div>
+          {
+           results.map(item => (
+             <CardElements
+               id={item.id}
+               key={item.id}
+               profilePicture={item.profilePicture}
+               description={item.description}
+               location={item.location}
+               wellnessArea={item.wellnessArea}
+             />
+           ))
+         }
+
+        </div>
+        </div>
+
     );
   }
 }
 
-export default BrowsProfiles;
+}
+export default  BrowsProfiles;

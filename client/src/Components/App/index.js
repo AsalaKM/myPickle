c0053227a822a9559d110b297c52490d5e36069a
Joe Friel <joseph.s.friel@gmail.com>
2019-01-07 16:54:14 +0000

    Add further styling to line up buttons and add links > rel issue #26

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import PageNotFound from "../Layouts/PageNotFound/index"
 import Register from "../Layouts/Register/index"
 import Login from "../Layouts/Login/index"
-import EditProfile from '../Layouts/EditProfile/index'
+import EditProfile from "../Layouts/EditProfile/index"
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
-      <Route path="/editProfile" exact component={EditProfile} />
+      <Route path="/edit-profile" exact component={EditProfile} />
 
       <Route component={PageNotFound} />
     </Switch>
--- client/src/Components/Layouts/EditProfile/index.js
+++ client/src/Components/Layouts/EditProfile/index.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react"
-import { EditContainer,BasicInfo,Button,TwoButton } from './EditProfile.style'
+import {
+  Header,
+  EditContainer,
+  BasicInfo,
+  TwoButton,
+  LeftButton,
+  RightButton,
+} from "./EditProfile.style"
 
-class  EditProfile extends Component {
-
-
-
+class EditProfile extends Component {
   render() {
     return (
       <EditContainer>
+        <Header>
+          <h2>Edit your profile</h2>
+        </Header>
 
-        <h2>Edit your profile</h2>
-
-        <BasicInfo>Basic Info</BasicInfo>
-        <br/>
+        <BasicInfo to={`/edit-profile/basic-info`}>
+          <p>Basic Info</p>
+        </BasicInfo>
+        <br />
         <TwoButton>
-        <Button>Support Details</Button>
-        <Button> Booking & Availability</Button>
-      </TwoButton>
-        <br/>
+          <LeftButton to={`/edit-profile/support-details`}>
+            <p>Support Details</p>
+          </LeftButton>
+          <RightButton to={`/edit-profile/availability-booking`}>
+            <p>Booking & Availability</p>
+          </RightButton>
+        </TwoButton>
+        <br />
         <TwoButton>
-        <Button>Target Audience</Button>
-        <Button>Social</Button>
+          <LeftButton to={`/edit-profile/target-audience`}>
+            <p>Target Audience</p>
+          </LeftButton>
+          <RightButton to={`/edit-profile/social-media`}>Social</RightButton>
         </TwoButton>
       </EditContainer>
-
     )
   }
 }

9df8cea52f193d6c2726ee468619fbbf0fd7e0b3
hshahwan <haneen.shahwan9@gmail.com>
2019-01-08 07:58:11 +0200

    delete browsprofiles and create single profile component

---
+++ client/src/Components/Layouts/SingleProfile/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import {  } from './BrowsProfiles.style';
+
+
+class SingleProfile extends Component {
+
+
+  render() {
+ <div>
+<section>basic info</section>
+<section>Bio</section>
+<section>buttons</section>
+<section>services</section>
+<section>Booking</section>
+<section>contact</section>
+</div>
+
+
+}
+export default SingleProfile;

b0db27dfbddee9491d8e0c0740a45d8625ce9037
Joe Friel <joseph.s.friel@gmail.com>
2019-01-08 17:19:19 +0000

    Make Dashboard name dynamic now using profileID in state

--- client/src/Components/Layouts/Dashboard/index.js
+++ client/src/Components/Layouts/Dashboard/index.js
@@ -1,9 +1,29 @@
-import React, { Component } from 'react';
-import { Button, Container, Logout } from './Dashboard.style';
+import React, { Component } from "react"
+import { Button, Container, Logout } from "./Dashboard.style"
 
-
+import jwt_decode from "jwt-decode"
 
 class Dashboard extends Component {
+  state = {
+    name: "",
+    loaded: false,
+  }
+
+  componentDidMount() {
+    if (localStorage.jwtToken) {
+      const decoded = jwt_decode(localStorage.jwtToken)
+      const name = decoded.name
+      const firstName = name.split(" ")[0]
+      this.setState({
+        name: `, ${firstName}`,
+        loaded: true,
+      })
+    } else {
+      this.setState({
+        loaded: true,
+      })
+    }
+  }
 
   editProfile = () => {
     this.props.history.push("/")
@@ -19,23 +39,29 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { loaded, name } = this.state
+
+    if (!loaded) {
+      return <h1>Loading your details...</h1>
+    }
+
     return (
       <Container>
-        <h2>Welcome back, Haneen</h2>
+        <h2>Welcome back{name}</h2>
 
-        <Button onClick={ this.editProfile }> Edit Profile </Button>
+        <Button onClick={this.editProfile}> Edit Profile </Button>
         <br />
-        <Button onClick={ this.createPlog }> Create Blog Post </Button>
+        <Button onClick={this.createPlog}> Create Blog Post </Button>
         <br />
         <Button onClick={this.browseProfiles}> Browse Profiles</Button>
         <br />
-        <Button onClick= { this.browsePosts}>Browse Blog Posts</Button>
+        <Button onClick={this.browsePosts}>Browse Blog Posts</Button>
         <br />
 
         <Logout>Logout</Logout>
       </Container>
-    );
+    )
   }
 }
 
-export default Dashboard;
+export default Dashboard

0448fd5290e80489b835c04c244d12cd9fd61e48
Joe Friel <joseph.s.friel@gmail.com>
2019-01-08 11:57:38 +0000

    Hash password > rel issue #71

--- server/database/queries/registerProfile.js
+++ server/database/queries/registerProfile.js
@@ -1,13 +1,14 @@
 const Profile = require("../models/Profile")
 
-const registerProfile = async (supportType, userId, approved) => {
+const registerProfile = async (supportType, newUser, approved) => {
+  const userId = newUser._id
   const validateProfile = await Profile.find({ user: userId })
 
   //check if User has already registered a profile
   if (!validateProfile || validateProfile.length === 0) {
     const newProfile = new Profile({
       supportType,
-      userId,
+      user: userId,
       approved,
     })
     await newProfile.save()

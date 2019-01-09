7e079257d7de2609694ce015bebc0edf53bcb892
Joe Friel <joseph.s.friel@gmail.com>
2019-01-09 16:06:25 +0000

    Wrap all private routes into common component

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import jwt_decode from "jwt-decode"
 import setAuthToken from "../../Utils/setAuthToken"
 
-// import components
+// import layouts
 import PageNotFound from "../Layouts/PageNotFound/index"
 import Register from "../Layouts/Register/index"
 import EditProfile from "../Layouts/EditProfile/index"
@@ -16,6 +16,9 @@ import EditSocialMedia from "../Layouts/EditProfile/SocialMedia"
 import EditBasicInfo from "../Layouts/EditProfile/BasicInfo"
 import Login from "../Layouts/Login"
 import Dashboard from "../Layouts/Dashboard"
+
+// import common components
+import PrivateRoute from "../Common/PrivateRoute/PrivateRoute"
 
 class App extends Component {
   state = {
@@ -66,40 +69,62 @@ class App extends Component {
     return (
       <Router>
         <Switch>
+          {/* Public routes to go here */}
           <Route path="/register" exact component={Register} />
           <Route path="/login" exact component={Login} />
-          <Route path="/edit-profile" exact component={EditProfile} profileId={profileId} />
-          <Route
+
+          {/* Private routes to go here */}
+          <PrivateRoute
+            path="/edit-profile"
+            exact
+            component={EditProfile}
+            profileId={profileId}
+            isAuthenticated={isAuthenticated}
+          />
+          <PrivateRoute
             path="/edit-profile/support-details"
             exact
             component={EditSupportDetails}
             profileId={profileId}
+            isAuthenticated={isAuthenticated}
           />
-          <Route
+          <PrivateRoute
             path="/edit-profile/target-clients"
             exact
             component={EditTargetClients}
             profileId={profileId}
+            isAuthenticated={isAuthenticated}
           />
-          <Route
+          <PrivateRoute
             path="/edit-profile/availability-booking"
             exact
             component={EditBookingDetails}
             profileId={profileId}
+            isAuthenticated={isAuthenticated}
           />
-          <Route
+          <PrivateRoute
             path="/edit-profile/social-media"
             exact
             component={EditSocialMedia}
             profileId={profileId}
+            isAuthenticated={isAuthenticated}
           />
-          <Route
+          <PrivateRoute
             path="/edit-profile/basic-info"
             exact
             component={EditBasicInfo}
             profileId={profileId}
+            isAuthenticated={isAuthenticated}
           />
-          <Route path="/dashboard" exact component={Dashboard} profileId={profileId} />
+          <PrivateRoute
+            path="/dashboard"
+            exact
+            component={Dashboard}
+            profileId={profileId}
+            isAuthenticated={isAuthenticated}
+          />
+
+          {/* Final route for pages they don't find */}
           <Route component={PageNotFound} />
         </Switch>
       </Router>

8b643e67e47092196df92fe1120e2873549d8052
hshahwan <haneen.shahwan9@gmail.com>
2019-01-09 13:30:05 +0200

    delete cosole logs that written for debugging

--- server/database/queries/getSingleProfile.js
+++ server/database/queries/getSingleProfile.js
@@ -1,19 +1,15 @@
 // load the mongo models
 const ProfileAnswer = require("../models/ProfileAnswer")
 const Profile = require("../models/Profile")
-console.log("hello");
+const Users = require("../models/User")
 
-// const profileID = '5c2c9c41d2a9fe38cc19d0c3'
 const getAnswersProfile = async profileID => {
-  console.log(profileID,'profileID');
 
   // get the profile with the ID
   // you need to do this to make sure the id you feed into the mongoose query below is an object not a string
-  const profile = await Profile.findById(profileID)
-  console.log(profile);
+const profile = await Profile.findById(profileID)
+const allAnswers = await ProfileAnswer.aggregate([
 
-
-  const allAnswers = await ProfileAnswer.aggregate([
     { $match: { profile: profile._id } },
     {
       $lookup: {
@@ -25,9 +21,7 @@ const getAnswersProfile = async profileID => {
     },
   ])
 
-  // const BasicInfo = await Users.find({_id:id},{name:1,phone:1,email:1})
-  // console.log(res);
-  return allAnswers
+  return allAnswers;
 }
 
 module.exports = getAnswersProfile

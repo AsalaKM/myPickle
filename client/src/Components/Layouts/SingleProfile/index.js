b640914beeb7931f68eb6cba2c12831c308dbaa6
hshahwan <haneen.shahwan9@gmail.com>
2019-01-11 21:47:48 +0200

    building QuestionAnswers function

--- client/src/Components/Layouts/SingleProfile/index.js
+++ client/src/Components/Layouts/SingleProfile/index.js
@@ -21,6 +21,7 @@ import {
 class SinflePforile extends Component {
   state = {
     answers: {},
+    loading: false,
   }
 
   componentDidMount() {
@@ -31,57 +32,80 @@ class SinflePforile extends Component {
       .get(`/single-profile/${id}`)
       // .then(result => this.setState({ answers: result.data }))
       .then(result => {
-        this.setState({ answers: result.data })
+        // console.log(Object.keys(this.state.answers))
+        // console.log(this.state.answers)
+        const questionsAnswers = Object.keys(result.data).map(element => {
+          console.log(result.data[element])
+
+          if (result.data[element].question[0].questionText == "haneen") {
+            return result.data[element].answers
+          }
+        })
+        this.setState({ answers: questionsAnswers, loading: true })
+        console.log(this.state.answers)
       })
       .catch(err => console.log(err))
   }
 
   render() {
-    // const { name, phone } = this.state.data.BasicInfo
+    // if (this.state.answers.BsicInfo) {
+    //   const BsicInfo = this.state.answers.BsicInfo[0]
+    //   console.log(BsicInfo.name)
+    // }
 
-    return (
-      <React.Fragment>
-        <BasicInfo>
-          <Avatar src={work} alt="" />
-          <Informations>
-            <h4> Haneen</h4>
-            <h4> Gaza</h4>
-            <h4>wellnes area</h4>
-            <button>Contact </button>
-          </Informations>
-        </BasicInfo>
+    const { answers, loading } = this.state
+    if (!loading) {
+      return <div>loading</div>
+    } else {
+      const BsicInfo = this.state.answers.BsicInfo[0]
 
-        <Bio>
-          <h2> Bio</h2>
-          Hello myPikle
-        </Bio>
-        <Navigate>
-          <Button>view blog</Button>
-          <Button>services</Button>
-          <Button>Booking</Button>
-        </Navigate>
-        <Services>
-          <help>
-            <h4> I have 7 years in practic</h4>
-            <h4>At a high level, what do i help with? </h4>
-            <button>Managing feelings & behaviours </button>
-            <button>Coping with life challenges</button>
-            <button>Mental illness diagnosis & treatment</button>
-          </help>
-          <Delivery>
-            <h4> Method of delivery </h4>
-            <button>Face-to-face therapy</button>
-            <button>Home visits therapy</button>
-          </Delivery>
-        </Services>
-        <Booking>
-          <h3>Booking </h3>
-          <h4>Cost per session (per hour): £80-99</h4>
-          <h4>Hours of operation </h4>
-        </Booking>
-        <Contact>Contact</Contact>
-      </React.Fragment>
-    )
+      // console.log(BsicInfo.values({ name: "Josephine Doeski" }))
+      // console.log(BsicInfo)
+
+      return (
+        <React.Fragment>
+          <BasicInfo>
+            <Avatar src={work} alt="" />
+            <Informations>
+              <h4> {BsicInfo.name} </h4>
+              <h4> Gaza</h4>
+              <h4>wellnes area</h4>
+              <button>Contact </button>
+            </Informations>
+          </BasicInfo>
+
+          <Bio>
+            <h2> Bio</h2>
+            Hello myPikle
+          </Bio>
+          <Navigate>
+            <Button>view blog</Button>
+            <Button>services</Button>
+            <Button>Booking</Button>
+          </Navigate>
+          <Services>
+            <help>
+              <h4> I have 7 years in practic</h4>
+              <h4>At a high level, what do i help with? </h4>
+              <button>Managing feelings & behaviours </button>
+              <button>Coping with life challenges</button>
+              <button>Mental illness diagnosis & treatment</button>
+            </help>
+            <Delivery>
+              <h4> Method of delivery </h4>
+              <button>Face-to-face therapy</button>
+              <button>Home visits therapy</button>
+            </Delivery>
+          </Services>
+          <Booking>
+            <h3>Booking </h3>
+            <h4>Cost per session (per hour): £80-99</h4>
+            <h4>Hours of operation </h4>
+          </Booking>
+          <Contact>Contact</Contact>
+        </React.Fragment>
+      )
+    }
   }
 }
 

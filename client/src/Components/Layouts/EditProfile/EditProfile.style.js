f7c055fc8e157e8210d7e917b3ad9f452cb6eccf
Joe Friel <joseph.s.friel@gmail.com>
2019-01-14 10:35:39 +0000

    Add profile links in edit profile

--- client/src/Components/Layouts/EditProfile/EditProfile.style.js
+++ client/src/Components/Layouts/EditProfile/EditProfile.style.js
@@ -13,7 +13,7 @@ export const EditContainer = styled.div`
   flex-direction: column;
   height: 100vh;
   padding: 1rem;
-  margin-top: 3rem;
+  margin-top: 2rem;
 `
 
 export const Button = styled(Link)`

f3b8b4f3dd460f83d84bceb2561b06aa772ba582
ismail2009 <somaaa2009@live.com>
2019-01-14 08:59:29 +0200

    fix testing

--- client/src/Components/Layouts/PageNotFound/PageNotFound.test.js
+++ client/src/Components/Layouts/PageNotFound/PageNotFound.test.js
@@ -1,9 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import PageNotFound from "./index"
+import { Error404, Error500 } from "./index"
 
 it("renders without crashing", () => {
   const div = document.createElement("div")
-  ReactDOM.render(<PageNotFound />, div)
+  ReactDOM.render(<Error404 />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it("renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<Error500 />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
